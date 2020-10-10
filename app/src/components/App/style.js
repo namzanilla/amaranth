c275ff275e0d79ea9aca3d8dceecd47d55297484
Ivan Akimov <namzanilla@gmail.com>
2020-10-10 11:15:19 +0300

    up: global styles

--- app/src/components/App/style.js
+++ app/src/components/App/style.js
@@ -11,9 +11,12 @@ export default createGlobalStyle`
   	margin: 0;
   	padding: 0;
   }
+  a {
+    text-decoration: none;
+  }
   body {
     background-color: #fff;
     color: #000;
-    font: normal normal normal 1em/1.5 verdana, sans-serif, serif;
+    font: normal normal normal 1em/1.5 arial, sans-serif;
   }
 `;
