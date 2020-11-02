9a0e84de39cae589990fada9630d76a9351c29c9
Ivan Akimov <namzanilla@gmail.com>
2020-11-02 17:22:21 +0200

    up: run scripts

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ killall -9 node
 
 sh -c "docker-compose start" &
 
+./static/js/build/clear.sh
+
 cd ./api || exit
 sh -c "npm run dev" &
 
--- run_production.sh
+++ run_production.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 killall -9 node
+
 sh -c "docker-compose start" &
+
+./static/js/build/clear.sh
+
 cd ./api || exit
 sh -c "npm start" &
+
 cd ../app || exit
 sh -c "npm run wp:build" &
 sh -c "npm start" &
