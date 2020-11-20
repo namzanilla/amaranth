f087705fc5bc7003608b9afebb5e1d124752071c
Ivan Akimov <namzanilla@gmail.com>
2020-11-20 15:41:42 +0200

    up: H1 component

--- app/src/components/H1/H1.js
+++ app/src/components/H1/H1.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {H1Wrap} from './style';
+import {
+  H1Wrap,
+  H1,
+} from './style';
 
 export default (props) => {
   return (
     <H1Wrap>
-      {props.child}
+      <H1>
+        {props.child}
+      </H1>
     </H1Wrap>
   );
 };
--- app/src/components/H1/style.js
+++ app/src/components/H1/style.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
-export const H1Wrap = styled.h1`
+export const H1Wrap = styled.div`
+  background: #FFE2E4;
+  margin-bottom: 20px;
+`;
+
+export const H1 = styled.h1`
   max-width: 1280px;
   margin: 0 auto;
   padding: 0 20px;
   font-size: 28px;
   color: #000;
+  line-height: 216%;
 `;
