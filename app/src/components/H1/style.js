f5fdc20a4f8cc25ad1e588274c3e845b993633ac
Ivan Akimov <namzanilla@gmail.com>
2020-11-20 16:10:11 +0200

    up: app / H1 component style

--- app/src/components/H1/style.js
+++ app/src/components/H1/style.js
@@ -9,7 +9,8 @@ export const H1 = styled.h1`
   max-width: 1280px;
   margin: 0 auto;
   padding: 0 20px;
-  font-size: 28px;
+  font-size: 24px;
   color: #000;
-  line-height: 216%;
+  line-height: 204%;
+  font-weight: normal;
 `;
