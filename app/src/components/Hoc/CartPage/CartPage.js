ce6904c202948d6e63043f851fbb4863c6de546e
Ivan Akimov <namzanilla@gmail.com>
2020-11-24 08:17:14 +0200

    up: app / Cart page loading

--- app/src/components/Hoc/CartPage/CartPage.js
+++ app/src/components/Hoc/CartPage/CartPage.js
@@ -1,30 +1,52 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Helmet} from 'react-helmet';
 import H1 from 'components/H1';
 import CartDetails from './CartDetails';
 
 export default (props) => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    props.fetchCartDetails();
+    props.fetchCartDetails().then(() => {
+      setLoading(false);
+    }).catch(() => {
+      setLoading(false);
+    });
   }, []);
+
+  let contentJSX = null;
+
+  if (!loading) {
+    contentJSX = (
+      <CartDetails
+        history={props.history}
+      />
+    );
+  }
 
   return (
     <>
       <Helmet>
-        <title>{getH1(props.languageId)}</title>
+        <title>{getTitle(props.languageId, loading)}</title>
       </Helmet>
-      <H1 child={getH1(props.languageId)} />
-      <CartDetails
-        history={props.history}
-      />
+      <H1 child={getH1(props.languageId, loading)} />
+      {contentJSX}
     </>
   );
 }
 
-const getH1 = (languageId) => {
-  if (1 === languageId) {
-    return 'Кошик';
-  } else if (2 === languageId) {
-    return 'Корзина';
+const getTitle = (languageId, loading) => {
+  if (loading) {
+    return languageId === 1
+      ? 'Завантаження...'
+      : 'Загрузка...';
   }
+
+  return languageId === 1
+    ? 'Кошик'
+    : 'Корзина';
+};
+
+const getH1 = (languageId, loading) => {
+  return getTitle(languageId, loading);
 };
