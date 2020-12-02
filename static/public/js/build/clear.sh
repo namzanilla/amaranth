416384f87d07df0a6b0721169e31bc0fc1da6f99
Ivan Akimov <namzanilla@gmail.com>
2020-12-02 09:49:33 +0200

    up: static / clear js build

--- static/public/js/build/clear.sh
+++ static/public/js/build/clear.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-#BASEDIR=$(dirname "$0")
-#echo "$BASEDIR"
-
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in, thus /home/user/bin
 SCRIPT_PATH=$(dirname "$SCRIPT")
+
 cd "$SCRIPT_PATH" || exit
+
 find . -name "*.gz" -type f -delete
 find . -name "*.js" -type f -delete
 find . -name "*.map" -type f -delete
