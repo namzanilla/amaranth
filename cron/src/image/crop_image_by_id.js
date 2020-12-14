d32dbea189a7f763c912189be4199b77cd1b9f74
Ivan Akimov <namzanilla@gmail.com>
2020-12-14 22:49:13 +0200

    up: cron / crop image by id

--- cron/src/image/crop_image_by_id.js
+++ cron/src/image/crop_image_by_id.js
@@ -1,23 +1,27 @@
 const sharp = require('sharp');
 const {resolve} = require('path');
 require('./../dotenv');
+const fs = require('fs');
+const IMAGE_TYPE_PRODUCT = 1;
+const IMAGE_TYPE_MODEL = 2;
+
 require('./../mysql')(async (connection) => {
   try {
     let {
       2: imageId,
-      3: width,
-      4: height,
+      // 3: width,
+      // 4: height,
     } = process.argv;
 
     imageId = parseInt(imageId);
-    width = parseInt(width);
-    height = parseInt(height);
+    // width = parseInt(width);
+    // height = parseInt(height);
 
     if (isNaN(imageId)) process.exit(0);
-    if (isNaN(width)) process.exit(0);
-    if (isNaN(height)) process.exit(0);
-    if (width < 1) process.exit(0);
-    if (height < 1) process.exit(0);
+    // if (isNaN(width)) process.exit(0);
+    // if (isNaN(height)) process.exit(0);
+    // if (width < 1) process.exit(0);
+    // if (height < 1) process.exit(0);
 
     const image = await getImage(connection, imageId);
 
@@ -30,28 +34,54 @@ require('./../mysql')(async (connection) => {
       0: {
         path,
         name,
+        type,
         fe_name,
       } = {},
     } = image;
 
-    const fileName = `${name}.${fe_name}`;
-    const newFileName = `${name}_${width}_${height}.${fe_name}`;
-    const dir = resolve(__dirname, '..', '..', '..', 'static', 'images', path);
-    const file = resolve(dir, fileName);
+    let outputDir, config;
+    const srcDir = resolve(__dirname, '..', '..', '..', 'static', 'images', path);
 
-    const fileOut = resolve(dir+'_crop', newFileName);
-    console.log('file', file);
+    if (type === IMAGE_TYPE_PRODUCT) {
+      outputDir = resolve(__dirname, '..', '..', '..', 'static', 'public', path);
+    } else if (type === IMAGE_TYPE_MODEL) {
+      outputDir = resolve(__dirname, '..', '..', '..', 'static', 'public', path);
+      fs.mkdirSync(outputDir, { recursive: true });
+      config = [
+        {
+          width: 320,
+          height: 320,
+        },
+        {
+          width: 480,
+          height: 480,
+        }
+      ];
 
-    await sharp(file)
-      .resize(width, height, {
-        position: 'centre',
-      })
-      .toFile(fileOut);
+    }
 
+    for (const conf of config) {
+      const {width, height} = conf;
+      const fileName = `${name}.${fe_name}`;
+      const newFileName = `${name}_${width}x${height}.${fe_name}`;
+      const file = resolve(srcDir, fileName);
+
+      const fileOut = resolve(outputDir, newFileName);
+      console.log('file', file);
+      console.log('fileOut', fileOut);
+
+      await sharp(file)
+        .resize(width, height, {
+          position: 'centre',
+        })
+        .toFile(fileOut);
+    }
 
     console.log(image);
   } catch (e) {
-    console.log(e);
+    console.error(e);
+
+    process.exit(0);
   }
 
   process.exit(0);
@@ -62,6 +92,7 @@ const getImage = (connection, imageId) => {
     const qs = `select
        i.name,
        i.path,
+       i.type,
        fe.name fe_name
     from image i
     inner join filename_extension fe
