bee739d3c7d7133aef767cf2a6d13f03e210020f
Ivan Akimov <namzanilla@gmail.com>
2020-12-14 23:03:01 +0200

    fix: app / H1 component style

--- app/src/components/H1/style.js
+++ app/src/components/H1/style.js
@@ -8,9 +8,8 @@ export const H1Wrap = styled.div`
 export const H1 = styled.h1`
   max-width: 1280px;
   margin: 0 auto;
-  padding: 0 20px;
+  padding: 8px 20px;
   font-size: 24px;
   color: #000;
-  line-height: 204%;
   font-weight: normal;
 `;
