f4a868c017b8c08fed8a2f01b71b409a87b06c66
Ivan Akimov <namzanilla@gmail.com>
2020-12-25 17:01:25 +0200

    up: api / brand service

--- api/src/services/brand.service.js
+++ api/src/services/brand.service.js
@@ -1,100 +1,135 @@
 const brandMetaKey = 'product_brand';
+const sh = require('../helpers/service');
 
 module.exports = (app) => {
-  const getMetaKeyId = async (metaKey) => {
-    const sql = `
+  return {
+    getBrandList: getBrandList(app),
+    getBrandById: getBrandById(app),
+    getModelList: getModelList(app),
+  };
+};
+
+function getModelList(app) {
+  const serviceName = 'brand:getModelList';
+
+  return async function(brandId) {
+    try {
+      const brand = await getBrandById(app)(brandId);
+
+      return sh.posResponse(serviceName, brand);
+    } catch (e) {
+      console.error(e);
+
+      return sh.negResponse(serviceName, e);
+    }
+  }
+}
+
+function getMetaKeyId(app) {
+  return async function(metaKey) {
+    try {
+      const sql = `
       SELECT
         mk.id
       FROM meta_key mk
       WHERE mk.name=?
     `;
 
-    return new Promise((resolve, reject) => {
-      app.mysql.connection.query(sql, metaKey, (error, results) => {
-        if (error) {
-          reject(error);
-        }
+      return new Promise((resolve, reject) => {
+        app.mysql.connection.query(sql, metaKey, (error, results) => {
+          if (error) {
+            reject(error);
+          }
 
-        const {
-          [0]: {
-            id,
-          } = {},
-        } = results;
+          const {
+            [0]: {
+              id,
+            } = {},
+          } = results;
 
-        resolve(id);
+          resolve(id);
+        });
       });
-    });
-  };
-
-  const getBrandById = async (brandId) => {
-    let metaKeyId
-    try {
-      metaKeyId = await getMetaKeyId(brandMetaKey);
     } catch (e) {
-      console.log(e);
-      return {};
+      console.error(e);
     }
-
-    if (!metaKeyId) return {};
-
-    const sql = `
-      SELECT
-        mv.id,
-        mv.name
-      FROM meta_value mv
-      WHERE mv.id=?
-      AND mv.meta_key_id=?
-    `;
-
-    return new Promise((resolve, reject) => {
-      app.mysql.connection.query(sql, [brandId, metaKeyId], (error, results) => {
-        if (error) {
-          reject(error);
-        }
-
-        const {
-          [0]: result = {},
-        } = results;
-
-        resolve(result);
-      });
-    });
-  };
-
-  const getBrandList = async () => {
-    let metaKeyId
-    try {
-      metaKeyId = await getMetaKeyId(brandMetaKey);
-    } catch (e) {
-      console.log(e);
-      return [];
-    }
-
-    if (!metaKeyId) return [];
-
-    const sql = `
-      SELECT
-        mv.id,
-        mv.name
-      FROM meta_value mv
-      WHERE mv.meta_key_id=?
-      ORDER BY mv.name
-    `;
-
-    return new Promise((resolve, reject) => {
-      app.mysql.connection.query(sql, metaKeyId, (error, results) => {
-        if (error) {
-          reject(error);
-        }
-
-        resolve(results);
-      });
-    });
   }
+}
 
-  return {
-    getBrandList,
-    getBrandById,
-  };
-};
+function getBrandById(app) {
+  return async function(brandId) {
+    try {
+      let metaKeyId
+      try {
+        metaKeyId = await getMetaKeyId(app)(brandMetaKey);
+      } catch (e) {
+        console.log(e);
+        return {};
+      }
 
+      if (!metaKeyId) return {};
+
+      const sql = `
+          SELECT
+            mv.id,
+            mv.name
+          FROM meta_value mv
+          WHERE mv.id=?
+          AND mv.meta_key_id=?
+        `;
+
+      return new Promise((resolve, reject) => {
+        app.mysql.connection.query(sql, [brandId, metaKeyId], (error, results) => {
+          if (error) {
+            reject(error);
+          }
+
+          const {
+            [0]: result = {},
+          } = results;
+
+          resolve(result);
+        });
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+function getBrandList(app) {
+  return async function() {
+    try {
+      let metaKeyId
+      try {
+        metaKeyId = await getMetaKeyId(app)(brandMetaKey);
+      } catch (e) {
+        console.log(e);
+        return [];
+      }
+
+      if (!metaKeyId) return [];
+
+      const sql = `
+          SELECT
+            mv.id,
+            mv.name
+          FROM meta_value mv
+          WHERE mv.meta_key_id=?
+          ORDER BY mv.name
+        `;
+
+      return new Promise((resolve, reject) => {
+        app.mysql.connection.query(sql, metaKeyId, (error, results) => {
+          if (error) {
+            reject(error);
+          }
+
+          resolve(results);
+        });
+      });
+    } catch(e) {
+      console.error(e);
+    }
+  }
+}
