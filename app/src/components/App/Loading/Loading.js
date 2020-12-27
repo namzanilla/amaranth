cc9dd30e59ac39aa07e6d556b5d9a669347e7002
Ivan Akimov <namzanilla@gmail.com>
2020-12-27 15:48:08 +0200

    up: app / App -> Loading component

--- app/src/components/App/Loading/Loading.js
+++ app/src/components/App/Loading/Loading.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import {LoadingWrap} from './style';
-import Spinner from 'components/Spinner';
+import React, {useEffect, useState} from 'react'
+import Spinner from 'components/Spinner'
+import {
+  LoadingWrap,
+  InnerLayer,
+} from './style'
 
-export default () => {
+export default (props) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [spinnerIsVisible, setSpinnerIsVisible] = useState(false)
+
+  useEffect(() => {
+    if (isVisible !== props.isVisible) {
+      setIsVisible(props.isVisible)
+
+      if (props.isVisible) {
+        
+        setSpinnerIsVisible(true)
+      }
+
+      if (!props.isVisible) {
+        setTimeout(() => setSpinnerIsVisible(false), 400)
+      }
+    }
+  }, [props.isVisible])
+
   return (
-    <LoadingWrap>
-      <Spinner />
+    <LoadingWrap
+      visibility={props.isVisible ? 'visible' : 'hidden'}
+    >
+      <InnerLayer
+        opacity={props.isVisible ? 1 : 0}
+      >
+        {spinnerIsVisible ? <Spinner /> : null}
+      </InnerLayer>
     </LoadingWrap>
-  );
+  )
 }
--- app/src/components/App/Loading/style.js
+++ app/src/components/App/Loading/style.js
@@ -2,11 +2,23 @@ import styled from 'styled-components';
 
 export const LoadingWrap = styled.div`
   position: fixed;
+  width: 100%;
+  top: 0;
+  bottom: 0;
+  z-index: 901;
+  visibility: ${(props) => props.visibility};
+  transition: visibility .3s linear 0s;
+`;
+
+export const InnerLayer = styled.div`
+  position: fixed;
   background: rgba(0, 0, 0, .7);
   width: 100%;
   top: 0;
   bottom: 0;
   z-index: 901;
+  opacity: ${(props) => props.opacity};
+  transition: opacity .3s linear;
   svg {
     position: absolute;
     top: 50%;
