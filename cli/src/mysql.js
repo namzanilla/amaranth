55578466d0dedc6eb08dbecddae81d0c777eed45
Ivan Akimov <namzanilla@gmail.com>
2020-12-27 20:14:49 +0200

    refactor: cli / mysql.js

--- cli/src/mysql.js
+++ cli/src/mysql.js
@@ -1,25 +1,18 @@
-const mysql = require('mysql');
-const {
-  MYSQL_DB_HOST,
-  MYSQL_DB_PORT,
-  MYSQL_DB_USER,
-  MYSQL_DB_PASSWORD,
-  MYSQL_DB_NAME,
-} = process.env;
+const mysql = require('mysql')
 
 module.exports = (cb) => {
   const pool = mysql.createPool({
     connectionLimit : 20,
-    host: MYSQL_DB_HOST,
-    port: MYSQL_DB_PORT,
-    user: MYSQL_DB_USER,
-    password: MYSQL_DB_PASSWORD,
-    database: MYSQL_DB_NAME,
-  });
+    host: process.env.MYSQL_DB_HOST,
+    port: process.env.MYSQL_DB_PORT,
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+  })
 
   pool.getConnection(function(err, connection) {
     if (err) throw err;
 
     cb(connection);
-  });
+  })
 }
